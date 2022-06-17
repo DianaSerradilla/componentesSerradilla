cc3efc338f83e2366238ea9ed3b22be37589ecbc
Diana Serradilla <dianas@itcsoluciones.com>
2022-06-17 17:02:29 -0300

    EntregableUno, aún faltan cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { InsideComponent } from './inside/inside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    InsideComponent
   ],
   imports: [
     BrowserModule
